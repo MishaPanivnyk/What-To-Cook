21fd824e273e918f048584347868613f6cecdbd1
MishaPanivnyk <panivnykm@gmail.com>
2024-12-17 20:57:33 +0200

    add components/FavoriteContent

--- src/components/FavoriteContent/FavoriteContent.styled.ts
+++ src/components/FavoriteContent/FavoriteContent.styled.ts
@@ -6,6 +6,8 @@ export const FavoriteContentContainer = styled.section`
   gap: 20px;
   padding: 60px;
   backdrop-filter: blur(20px);
+  flex-wrap: wrap;
+  position: relative;
   box-shadow: 0px 0px 30px rgba(227, 228, 237, 0.37);
   border: 2px solid rgba(255, 255, 255, 0.18);
   @media only screen and (min-width: 768px) {
@@ -13,4 +15,10 @@ export const FavoriteContentContainer = styled.section`
 
   @media only screen and (min-width: 1440px) {
   }
+`;
+export const LoaderWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
--- src/components/FavoriteContent/FavoriteContent.tsx
+++ src/components/FavoriteContent/FavoriteContent.tsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { FavoriteContentContainer } from './FavoriteContent.styled';
+import {
+  FavoriteContentContainer,
+  LoaderWrapper,
+} from './FavoriteContent.styled';
 import { RecipeCard } from 'components/RecipeCard/RecipeCard';
 import { Loader } from 'components/Loader/Loader';
+
+const apiUrl = import.meta.env.VITE_API_URL;
 
 type Recipe = {
   id: number;
@@ -18,9 +23,7 @@ export const FavoriteContent: React.FC = () => {
   useEffect(() => {
     const fetchRecipes = async () => {
       try {
-        const response = await axios.get<Recipe[]>(
-          'http://127.0.0.1:8000/api/recipes/'
-        );
+        const response = await axios.get<Recipe[]>(`${apiUrl}/recipes/`);
         setRecipes(response.data);
       } catch (err: unknown) {
         if (axios.isAxiosError(err)) {
@@ -38,23 +41,26 @@ export const FavoriteContent: React.FC = () => {
     fetchRecipes();
   }, []);
 
-  if (loading) {
-    return <Loader />;
-  }
-
-  if (error) {
-    return <div>{error}</div>;
-  }
-
   return (
     <FavoriteContentContainer>
-      {recipes.map(recipe => (
-        <RecipeCard
-          key={recipe.id}
-          title={recipe.title}
-          imageUrl={recipe.photo}
-        />
-      ))}
+      {loading && (
+        <LoaderWrapper>
+          <Loader />
+        </LoaderWrapper>
+      )}
+      {error && <div>{error}</div>}
+      {!loading && !error && recipes.length === 0 && (
+        <div>No recipes available.</div>
+      )}
+      {!loading &&
+        !error &&
+        recipes.map(recipe => (
+          <RecipeCard
+            key={recipe.id}
+            title={recipe.title}
+            imageUrl={recipe.photo}
+          />
+        ))}
     </FavoriteContentContainer>
   );
 };
