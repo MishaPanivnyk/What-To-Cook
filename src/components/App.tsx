6b044b7e8d35093fcb6a664fc0f4b63a429896de
MishaPanivnyk <panivnykm@gmail.com>
2024-12-17 20:58:13 +0200

    add new Route

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,6 +9,8 @@ import AuthPage from 'pages/Auth/Auth';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import SearchPage from 'pages/Search/Search';
+import RecipesCreate from 'pages/RecipesCreate/RecipesCreate';
+import RecipesGenerate from 'pages/RecipesGenerate/RecipesGenerate';
 const App: React.FC = () => {
   return (
     <>
@@ -18,7 +20,15 @@ const App: React.FC = () => {
           path="/auth"
           element={<PublicRoute component={<AuthPage />} />}
         />
-        <Route path="/" element={<PrivateRoute component={<MainPage />} />} />
+        <Route path="/" element={<PrivateRoute component={<MainPage />} />} />{' '}
+        <Route
+          path="/recipes-generate"
+          element={<PrivateRoute component={<RecipesGenerate />} />}
+        />
+        <Route
+          path="/recipes-create"
+          element={<PrivateRoute component={<RecipesCreate />} />}
+        />
         <Route
           path="/search"
           element={<PrivateRoute component={<SearchPage />} />}
